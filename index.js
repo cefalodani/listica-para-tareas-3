094094e82875daea129e76d88d889e109c14c0b2
Daniel Cefalo <cefalo@MacBook-Pro-de-alejandri.local>
2023-05-11 12:31:49 -0400

    me falta solo una cosa

--- index.js
+++ index.js
@@ -26,13 +26,20 @@ function renderizarListaDeContactos() {
     li.appendChild(telefonoSpan);
 
     const editarBoton = document.createElement('button');
-    editarBoton.textContent = 'Editar';
-    
+    editarBoton.innerHTML = 
+`<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="editIcon ">
+<path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+</svg>
+`  
+  
     editarBoton.addEventListener('click', () => editarContacto(i));
     li.appendChild(editarBoton);
 
     const borrarBoton = document.createElement('button');
-    borrarBoton.textContent = 'Borrar';
+    borrarBoton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="editCheck">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+  </svg>
+  `
     borrarBoton.addEventListener('click', () => borrarContacto(i));
     li.appendChild(borrarBoton);
 
@@ -102,9 +109,11 @@ function cargarContactos() {
 
 // Cargamos los contactos al cargar la pagina
 cargarContactos();
+console.log(listaDeContactos.children.length);
 
 // Agregamos el evento submit al formulario
 formulario.addEventListener('submit', (evento) => {
   evento.preventDefault();
   validarFormulario();
 });
+
